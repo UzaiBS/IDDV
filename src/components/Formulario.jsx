1bc5929970f32786ecefd189cfea5376702a2628
Uzai Baltazar <uzai.baltazar23b@udgvirtual.udg.mx>
2024-02-10 18:31:57 -0600

    Actualización de iddv

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,5 +1,6 @@
 import "./Formulario.css"
 import { useState } from "react"
+import { FaUser, FaLock } from "react-icons/fa";
 
 export function Formulario({setUser}){
     const   [nombre, setNombre] = useState("")
@@ -16,17 +17,32 @@ export function Formulario({setUser}){
         <section>            
             <form className= "formulario" onSubmit= {manejoEnvio}>
                 
-                <h1>IDDV</h1>                
-                <input type="email" placeholder='Correo' required 
-                    value={nombre}
-                    onChange= {e => setNombre(e.target.value)}
-                />
-                <input type="password" placeholder='Contraseña' required 
-                    value={contrasenia}
-                    onChange={e=> setContrasenia(e.target.value)}
-                />
+                <h1>IDDV</h1>  
+
+                <div className="campo">
+                    <input type="email" placeholder='Correo' required 
+                        value={nombre}
+                        onChange= {e => setNombre(e.target.value)}
+                    />
+                    <FaUser className= "icono" />
+                </div>                
+                <div className="campo">
+                    <input type="password" placeholder='Contraseña' required 
+                        value={contrasenia}
+                        onChange={e=> setContrasenia(e.target.value)}
+                    />
+                    <FaLock className= "icono" />
+                </div>
+                <div className="recordar_contrasenia">
+                    <a href="#">Olvidé mi contraseña</a>
+                </div>
 
                 <button>Iniciar sesión</button>
+
+                <div className="enlace">
+                    <p>¿Desea registrarse? <a href="#">Iniciar registro</a></p>
+                </div>
+                
             </form>
         </section>
     )
